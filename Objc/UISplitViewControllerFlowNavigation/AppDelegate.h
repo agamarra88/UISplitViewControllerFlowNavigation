bc809a035250006917b3192066563d18f6465de2
Arturo Gamarra <agamarra@belatrixsf.com>
2017-10-11 07:54:43 -0500

    organizing files

---
+++ Objc/UISplitViewControllerFlowNavigation/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UISplitViewControllerFlowNavigation
+//
+//  Created by Arturo Gamarra on 10/11/17.
+//  Copyright © 2017 Abstract. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Objc/UISplitViewControllerFlowNavigation/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  UISplitViewControllerFlowNavigation
+//
+//  Created by Arturo Gamarra on 10/11/17.
+//  Copyright © 2017 Abstract. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
